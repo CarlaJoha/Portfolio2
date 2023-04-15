b9ba2fd83adf38d312e60ceb2ce51be1740c27df
carlajoha <carlajoha.work@gmail.com>
2023-04-14 20:35:42 -0400

    prueba

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About() {
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="./Carla.svg"
+                src="../../public/Carla.svg"
               />
             </div>
           </div>
