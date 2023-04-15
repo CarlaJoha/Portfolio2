5210b212d5da0f5339447dc1a24b3c3efa101207
carlajoha <carlajoha.work@gmail.com>
2023-04-14 20:52:21 -0400

    prueba

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ export default function About() {
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
               <img
-                className="object-cover object-center rounded"
+                className="w-70 rounded-full flex-shrink-0 object-cover object-center"
                 alt="hero"
                 src="../../public/Carla.svg"
               />
